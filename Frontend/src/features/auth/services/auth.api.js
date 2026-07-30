7a89b6ff69fe84dce94d755c3bd042b41c7c67f5
ArvindChandak061 <maasaraswati654@gmail.com>
2026-07-30 22:27:05 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-analyser-and-builder-backend-x6b3.onrender.com",
     withCredentials: true
 })
 
