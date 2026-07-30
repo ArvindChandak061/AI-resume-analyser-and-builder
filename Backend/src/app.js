e924f2c852e981a9162550fdc94b3d3dc11d48e7
Arvind-Chandak <arvindchandak061@gmail.com>
2026-07-30 22:21:07 +0530

    Your commit message

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,20 +6,31 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+// Put all allowed URLs in an array
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL // Will read from hosting settings
+].filter(Boolean) // Cleans up empty values
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: function (origin, callback) {
+    // Allow Postman/mobile tools or matching origins
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("CORS error: Not allowed"))
+    }
+  },
+  credentials: true
 }))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
